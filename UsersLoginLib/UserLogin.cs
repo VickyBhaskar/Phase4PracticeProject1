c17d374513f6d1f7297655e3d10bb880cdfb2f92
VickyBhaskar <vicky2400kumar@gmail.com>
2024-01-17 12:30:49 +0530

    initial Commit

---
+++ UsersLoginLib/UserLogin.cs
@@ -0,0 +1,19 @@
+﻿namespace UsersLoginLib
+{
+    public class UserLogin
+    {
+        public string Login(string userName, string userPwd)
+        {
+            string result = "";
+            if (userName == "Vicky" && userPwd == "Vicky@68")
+            {
+                result = "Login success";
+            }
+            else
+            {
+                result = "Login Failed";
+            }
+            return result;
+        }
+    }
+}
